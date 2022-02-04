5b7579fe57bd3e6f2c11eb5862d47e9d84c5e723
Joakim Bugge <joakim.bugge@gmail.com>
2022-02-04 23:29:39 +0100

    feat: add onEachComplete event to forSeeders()

--- test/seeder/builders/runSeeders.spec.ts
+++ test/seeder/builders/runSeeders.spec.ts
@@ -1,4 +1,4 @@
-import { BaseSeeder, Seeder } from '../../../src';
+import { BaseSeeder, Seeder, SeederConstructor } from '../../../src';
 import { runSeeders } from '../../../src/seeder/builders/runSeeders';
 import { toSeederEntry } from '../../../src/seeder/utils/toSeederEntry';
 
@@ -9,6 +9,24 @@ describe(runSeeders.name, () => {
     const completedSeeders = await runSeeders(entries);
 
     expect(completedSeeders).toEqual(seeders);
+  });
+
+  it('calls onEachComplete after each completion', async () => {
+    const callback = jest.fn((seeder: SeederConstructor) => seeder);
+
+    const seeders = [SeederMock, SecondSeederMock, ThirdSeederMock];
+    const entries = seeders.map(toSeederEntry);
+
+    await runSeeders(entries, {
+      onEachComplete(seeder) {
+        callback(seeder);
+      },
+    });
+
+    expect(callback.mock.calls).toHaveLength(3);
+    expect(callback.mock.calls[0][0]).toEqual(seeders[0]);
+    expect(callback.mock.calls[1][0]).toEqual(seeders[1]);
+    expect(callback.mock.calls[2][0]).toEqual(seeders[2]);
   });
 });
 
