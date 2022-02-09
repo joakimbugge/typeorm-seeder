89e59a90aa571a597a534a624a10c3f5ce7df0ea
Joakim Bugge <joakim.bugge@gmail.com>
2022-02-09 18:26:59 +0100

    refactor(utils): remove unused try-catch (#87)

--- src/entity/utils/getConnectionForEntity.ts
+++ src/entity/utils/getConnectionForEntity.ts
@@ -3,13 +3,9 @@ import { EntityConnectionError } from '../../errors';
 import { Entity } from '../models/Entity';
 
 export function getConnectionForEntity<T>(entity: Entity<T>): Connection {
-  const connection = getConnectionManager().connections.find((connection) => {
-    try {
-      return connection.getMetadata(entity).target == entity && connection.isConnected;
-    } catch (error) {
-      return undefined;
-    }
-  });
+  const connection = getConnectionManager().connections.find(
+    (connection) => connection.getMetadata(entity).target == entity && connection.isConnected,
+  );
 
   if (!connection) {
     throw new EntityConnectionError(entity);
